bfb4f8afcea28caa9f7a3cc3c6ddcb549e35dde5
Steve Wong <steve@42puzzle.com>
2016-04-09 04:40:11 +0000

    Initial Commit

---
+++ config/boot.rb
@@ -0,0 +1,12 @@
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host:  '192.168.33.33', Port: 3000)
+      #super.merge(Host:  '127.0.0.1', Port: 3000)
+    end
+  end
+end
